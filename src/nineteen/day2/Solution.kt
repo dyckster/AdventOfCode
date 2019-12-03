061260fbc20293e658bf5af5dceace6dee0f6f30
dyckster <duckstersoni@gmail.com>
2019-12-03 12:16:24 +0300

    day 2 solution
    add day 3 puzzle input

--- src/nineteen/day2/Solution.kt
+++ src/nineteen/day2/Solution.kt
@@ -8,6 +8,7 @@ private const val DELIMITER = ","
 private const val CODE_ADD = 1
 private const val CODE_MULTIPLY = 2
 private const val CODE_FINISH = 99
+private const val STEP_OFFSET = 4
 
 private val OP_CODES = arrayOf(CODE_ADD, CODE_MULTIPLY, CODE_FINISH)
 
@@ -17,41 +18,40 @@ fun main(args: Array<String>) {
             .split(DELIMITER)
             .map { it.trim().toInt() }
             .toTypedArray()
+    println(Arrays.toString(input))
+
     var changedArray = input
-    for (i in input.withIndex()) {
-        if (changedArray[i.index] == CODE_FINISH) {
-            println("Finishing...")
+
+    //Apply preconditions
+    changedArray[1] = 12
+    changedArray[2] = 2
+
+    for (i in 0..input.size) {
+        val currVal = changedArray[i]
+
+        if (i % STEP_OFFSET != 0) continue
+        if (currVal == CODE_FINISH) {
+            println("Halting program...")
             break
         }
-        if (changedArray[i.index] % 4 != 0) continue
-        val code = changedArray[i.index]
-        val firstPos = changedArray[i.index + 1]
-        val secondPos = changedArray[i.index + 2]
-        val finalPos = changedArray[i.index + 3]
 
-        println("Code: $code")
-//        println("First value position: $firstPos")
-//        println("Second value position: $secondPos")
-//        println("Final value position: $finalPos")
-
-        changedArray = when (code) {
-            CODE_ADD -> addValues(firstPos, secondPos, finalPos, changedArray)
-            CODE_MULTIPLY -> multiplyValues(firstPos, secondPos, finalPos, changedArray)
-            else -> changedArray
-        }
+        changedArray = addValues(currVal, changedArray[i + 1], changedArray[i + 2], changedArray[i + 3], changedArray)
     }
-    println(Arrays.toString(input))
+
     println(Arrays.toString(changedArray))
 }
 
 
-private fun addValues(firstPos: Int, secondPos: Int, resultPos: Int, array: Array<Int>): Array<Int> {
-    array[resultPos] = array[firstPos].plus(array[secondPos])
-    return array
-}
+private fun addValues(code: Int, firstPos: Int, secondPos: Int, resultPos: Int, array: Array<Int>): Array<Int> {
+    val first = array[firstPos]
+    val second = array[secondPos]
+    val result = when (code) {
+        CODE_ADD -> first + second
+        CODE_MULTIPLY -> first * second
+        else -> throw IllegalArgumentException("Unsupported code! Must be one of: ${Arrays.toString(OP_CODES)}")
+    }
 
+    array[resultPos] = result
 
-private fun multiplyValues(firstPos: Int, secondPos: Int, resultPos: Int, array: Array<Int>): Array<Int> {
-    array[resultPos] = array[firstPos].times(array[secondPos])
     return array
 }
---
+++ src/nineteen/day3/Solution.kt
@@ -0,0 +1,5 @@
+package nineteen.day3
+
+fun main(args: Array<String>) {
+    
+}
